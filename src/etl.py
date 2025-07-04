46985708c9aefc22df8df7a99a9e81f3d9757a2b
Guilherme Portugal <flordavidaho@gmail.com>
2025-07-04 06:36:46 -0300

    merge dataframe

--- src/etl.py
+++ src/etl.py
@@ -1,24 +1,103 @@
 import pandas as pd
-import numpy as np 
+import os
 
 
 path = 'data/'
 
-print(path)
 
-def load_all_data(raw_data_path):
+
+def load_all_data(raw_data_path: str) -> dict:
     # Lógica para carregar todos os CSVs da pasta raw
     # e retornar um dicionário de DataFrames
-    pass
+    """
+    Carrega todos os arquivos .csv de uma pasta em um dicionário de DataFrames.
+    
+    O nome do arquivo (sem externção) se torna a chave do dicionário. 
+
+    Args:
+        raw_data_path (str): Caminho para pasta RAW, contendo os arquivos CSV.
+
+    Returns:
+        dict: Retorna um dicionário onde as chaves são os nomes dos arquivos e os valores são os DataFrames.
+    """
+    dataframes_dict = {}
+    n_arquivos = 0
+    print('Iniciando carregamento de dados brutos ...')
+    
+    try:
+        for arquivo in os.listdir(raw_data_path):
+            if arquivo.endswith('.csv'):
+                nome_df = arquivo.replace('.csv', '').replace('olist_', '').replace('_dataset', '')
+                caminho_completo = os.path.join(raw_data_path, arquivo)
+                dataframes_dict[nome_df] = pd.read_csv(caminho_completo)
+                print(f'    - Arquivo "{arquivo}" carregado como "{nome_df}" em {caminho_completo[:-7]}.')
+                n_arquivos += 1
+                
+    except FileNotFoundError:
+        print(f'ERRO: O diretório especificado não foi possível ser encontrado. - {raw_data_path}')
+        return None
+    
+    print(f"Carregamento de {n_arquivos} dados brutos concluído. \n")
+    
+    return dataframes_dict
 
 
-def merge_data(dataframes_dict):
+def merge_data(dataframes_dict: dict) -> pd.DataFrame:
     # Lógica para unir dataframes (orders, items, products, customers, payments, reviews ...)
     # return master_df
+    """
+    Une e transforma os DataFrames em um único DataFrame para análise
+
+    Args:
+        dataframes_dict (dict): Dicionário de DataFrames brutos.
+        
+    Returns:
+        pd.DataFrame: Um único DataFrame, porem ainda bruto.
+    """
+    if not dataframes_dict:
+        print("ERRO: Dicionário de dataframes está vazio. Transformação cancelada.")
+        return None
+    
+    print("Iniciando modelagem de dados ...")
+    
+    # --- Merge nos DataFrames ---
+    
+    # Tabela de 'pedidos' merged com 'itens_pedido'
+    df = pd.merge(dataframes_dict['orders'], dataframes_dict['orders_items'], on='order_id', how='left')
+    
+    # Adicionar informações dos produtos
+    df = pd.merge(dataframes_dict['products'], on='products_id', how='left')
+    
+    # Adicionar informações dos clientes
+    df = pd.merge(dataframes_dict['customers'], on='customer_id', how='left')
+    
+    # Adicionar informações dos vendedores
+    df = pd.merge(dataframes_dict['sellers'], on='seller_id', how='left')
+    
+    # Adicionar informações de pagamento
+    df = pd.merge(dataframes_dict['order_payments'], on='order_id', how='left')
+    
+    # Adicionar a tradução da categoria do produto
+    df = pd.merge(dataframes_dict['product_category_name_translation'], on='product_category_name', how='left')
+    
+    print("Merge de tabelas concluída.")
     pass
 
 
-def clean_and_transform_data(master_df):
+def clean_and_transform_data(master_df: pd.DataFrame) -> pd.DataFrame:
     # Lógica para converter conlunas de dadta, tratar valores nulos,
     # Criar novas colunas ( ex: tempo_de_entrega )
+    """
+    Transforma os valores do DataFrame único.
+
+    Args:
+        master_df (DataFrame): DataFrame unido, bruto.
+        
+    Returns:
+        pd.DataFrame: Um único DataFrame limpo e pronto para análise
+    """
+    
+    # df_columns = ['order_id' 'customer_id' 'order_status' 'order_purchase_timestamp' 
+    # 'order_approved_at' 'order_delivered_carrier_date'
+    # 'order_delivered_customer_date' 'order_estimated_delivery_date']
     pass
