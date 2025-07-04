cd500d3f41b9771423d4eead88f5c3ec43a50840
Guilherme Portugal <flordavidaho@gmail.com>
2025-07-03 21:43:30 -0300

    Create etl.py

---
+++ src/etl.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np 
+
+
+path = 'data/'
+
+print(path)
+
+def load_all_data(raw_data_path):
+    # Lógica para carregar todos os CSVs da pasta raw
+    # e retornar um dicionário de DataFrames
+    pass
+
+
+def merge_data(dataframes_dict):
+    # Lógica para unir dataframes (orders, items, products, customers, payments, reviews ...)
+    # return master_df
+    pass
+
+
+def clean_and_transform_data(master_df):
+    # Lógica para converter conlunas de dadta, tratar valores nulos,
+    # Criar novas colunas ( ex: tempo_de_entrega )
+    pass
